bb58a1c54d89d53965a722963171c1aed4e9a363
Noah D'Arcy <ndarcy0408@gmail.com>
2024-02-07 11:27:41 -0500

    404 page created and routed correctly

---
+++ frontend/app/not-found.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import { Navbar } from "./navbar";
+
+function notFound() {
+  return (
+    <>
+      <Navbar />
+      <div>NOt Found</div>
+    </>
+  );
+}
+
+export default notFound;
