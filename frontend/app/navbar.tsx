7c1914c43e73bd92380f355d066fae5ff80fae93
Noah D'Arcy <ndarcy0408@gmail.com>
2024-02-17 16:39:08 -0500

    Navigation bar integrated

--- frontend/app/navbar.tsx
+++ frontend/app/navbar.tsx
@@ -1,42 +1,59 @@
 "use client";
 
 import Link from "next/link";
+import Image from "next/image";
 import { motion } from "framer-motion";
 import { usePathname } from "next/navigation";
 
-const links = [{ href: "/about", text: "About" }];
+interface navLink {
+  href: string;
+  text: string;
+}
+
+const links = [
+  { href: "/", text: "Home" },
+  { href: "/products", text: "Products" },
+  { href: "/about", text: "About" },
+  { href: "/contact", text: "Contact" },
+];
 
 export const Navbar = () => {
   const path = usePathname();
   return (
-    <motion.div
-      initial={{ opacity: 0 }}
-      animate={{ opacity: 1 }}
-      exit={{ opacity: 0, transition: { duration: 15 } }}
-      className="container mx-auto"
-    >
-      <nav className="navbar bg-yellow-100 text-gray-800 mb-5">
-        <ul className="flex justify-center py-5  rounded-sm">
-          {links.map((l: { href: string; text: string }) => {
-            console.log(l.href);
-            console.log(path);
-            return (
-              <li className="pr-[2.5rem]" key={l.href}>
-                <motion.div>
-                  <Link
-                    className={`${
-                      l.href === path ? "text-yellow-600 font-bold" : ""
-                    } text-base`}
-                    href={l.href}
-                  >
-                    {l.text}
-                  </Link>
-                </motion.div>
-              </li>
-            );
-          })}
-        </ul>
-      </nav>
-    </motion.div>
+    <nav className="text-gray-800 mb-5 flex justify-between px-[2rem] md:px-[5.1875rem] py-[1.06rem] md:gap-[7.5rem]">
+      <Image
+        src={"/menu.svg"}
+        alt={"The Mobile Menu button, cool eh?"}
+        width={34}
+        height={34}
+        className="lg:hidden"
+      />
+      <Link href={"/"} className="md:mr-auto lg:mr-0">
+        <Image
+          src={"/tnt-logo.svg"}
+          alt={"The Toad and Turnip Logo in SVG"}
+          width={100}
+          height={80}
+        />
+      </Link>
+      <ul className="hidden lg:flex justify-center items-center">
+        {links.map((l: navLink) => {
+          return (
+            <li className="pr-[2.5rem]" key={l.href}>
+              <Link className={`font-sans`} href={l.href}>
+                {l.text}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+      <Image
+        src={"/bag.svg"}
+        alt={"Your shopping bag, because the shopping cart is too overused"}
+        width={38}
+        height={38}
+        className="hover:cursor-pointer"
+      />
+    </nav>
   );
 };
